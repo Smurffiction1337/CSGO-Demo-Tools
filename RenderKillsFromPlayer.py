4c9ab9b3f9f134eff962d10b40cb7faf6b7380a6
Smurffiction <63471314+Smurffiction1337@users.noreply.github.com>
2023-05-13 09:40:27 +0200

    Update RenderKillsFromPlayer.py

--- RenderKillsFromPlayer.py
+++ RenderKillsFromPlayer.py
@@ -3,6 +3,9 @@
 
 # Parse Demo for Kills of an certain player and renders the scene with x seconds infront and afetrwards
 # Custom settings: Resolution, Framerate, Target Player, add time before and after kill
+# Make sure to install the demoinfocs and ffmpeg libraries before running this script. You can install demoinfocs using pip
+# pip install demoinfocs
+
 
 import demoinfocs
 import subprocess
@@ -29,37 +32,6 @@ def render_video(segment_start, segment_end, output_file):
         '-vf', f'select=gte(n\\,{segment_start * fps})\\,lte(n\\,{segment_end * fps})',
         '-s', resolution,
         '-r', str(fps),
-        '-c:v', 'libx264',
-        '-crf', '18',
-        '-preset', 'slow',
-        '-pix_fmt', 'yuv420p',
-        output_file
-    ]
-    subprocess.run(command)
-
-def extract_audio(output_file):
-    # Use FFmpeg to extract the audio from the CS:GO demo
-    command = [
-        'ffmpeg',
-        '-i', demo_file,
-        '-vn',
-        '-acodec', 'copy',
-        output_file
-    ]
-    subprocess.run(command)
-
-def merge_audio_video(video_file, audio_file, output_file):
-    # Use FFmpeg to merge the video and audio files
-    command = [
-        'ffmpeg',
-        '-i', video_file,
-        '-i', audio_file,
-        '-c:v', 'copy',
-        '-c:a', 'aac',
-        '-strict', '-2',
-        '-map', '0:v:0',
-        '-map', '1:a:0',
-        '-shortest',
         output_file
     ]
     subprocess.run(command)
@@ -78,16 +50,7 @@ for game_event in demo.game_events:
             # The target player's team won the round, so they made the last kill
             segment_start = max(0, game_event.time - time_before)
             segment_end = min(demo.header.playback_ticks / demo.header.tick_rate, game_event.time + time_after)
-            output_video_file = f'kill_{game_event.time}.mp4'  # Use the kill time as the video file name
-            output_audio_file = f'kill_{game_event.time}.aac'  # Use the kill time as the audio file name
-            output_merged_file = f'kill_{game_event.time}_merged.mp4'  # Use the kill time as the merged file name
+            output_file = f'kill_{game_event.time}.mp4'  # Use the kill time as the video file name
 
             # Render the video segment
-            render_video(segment_start, segment_end, output_video_file)
-
-            # Extract the audio from the CS:GO demo
-            extract_audio(output_audio_file)
-
-            # Merge the video and audio files
-
-````
+            render_video(segment_start, segment_end, output_file)
